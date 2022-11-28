f6cb4fb6d5538fb2be5fdd7d3ad392b490802da2
Jurandi Riqueti Junior <jurandihd4000@gmail.com>
2022-11-28 00:15:28 -0300

    Adicionado todos os planetas

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-import { TextureLoader } from 'three';
+import { Mesh, TextureLoader } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 const renderer = new THREE.WebGLRenderer();
@@ -20,7 +20,7 @@ camera.lookAt(0, 0, 0);
 const scene = new THREE.Scene();
 const textureLoader = new THREE.TextureLoader();
 const clock = new THREE.Clock;
-const objectsDefinition = 10;
+const objectsDefinition = 64;
 //-----------------------------------------------------------------------
 
 //objects
@@ -30,34 +30,68 @@ const starsObj = {
 }
 
 const sunObj = {
-    radius: 13.91000/2,
+    radius: 6.95000,
     rotation: 0.01,
     texture: '../img/sun/sun_texture.jpg'
 }
 
 const mercuryObj = {
-    radius: 0.04880/2,
+    radius: 0.02439,
     rotation: 0.01,
     texture: '../img/mercury/mercury_texture.jpg'
 }
 
 const venusObj = {
-    radius: 0.12104/2,
+    radius: 0.06051,
     rotation: 0.01,
     texture: '../img/venus/venus_atmosphere_texture.jpg'
 }
 
 const earthObj = {
-    radius: 0.12756/2,
-    radiusCloud: (0.12756/2) + 0.001,
+    radius: 0.06378,
+    radiusCloud: 0.06378 + 0.001,
+    radiusMoon: 0.01737,
     rotation: 0.01,
     rotationCloud: 0.02,
     texture: '../img/earth/earth_day_texture.jpg',
+    textureMoon: '../img/earth/moon_texture.jpg',
     textureCloud: '../img/earth/earth_cloud_texture.png',
     normalMap: '../img/earth/earth_normal_map.tif',
     specularMap: '../img/earth/earth_specular_map.tif'
 }
 
+const marsObj = {
+    radius: 0.03397,
+    rotation: 0.01,
+    texture: '../img/mars/mars_texture.jpg'
+}
+
+const jupiterObj = {
+    radius: 0.71492,
+    rotation: 0.01,
+    texture: '../img/jupiter/jupiter_texture.jpg'
+}
+
+const saturnObj = {
+    radius: 0.60268,
+    rotation: 0.01,
+    radiusRingInside: 0.65232,
+    radiusRingOutside: 1.38232,
+    texture: '../img/saturn/saturn_texture.jpg',
+    textureRing: '../img/saturn/saturn_ring_texture2.png'
+}
+
+const uranusObj = {
+    radius: 0.25559,
+    rotation: 0.01,
+    texture: '../img/uranus/uranus_texture.jpg'
+}
+
+const neptuneObj = {
+    radius: 0.24746,
+    rotation: 0.01,
+    texture: '../img/neptune/neptune_texture.jpg'
+}
 //-----------------------------------------------------------------------
 
 //Milk Way
@@ -115,7 +149,77 @@ const cloudMaterial = new THREE.MeshBasicMaterial({
 const cloud = new THREE.Mesh(cloudGeometry, cloudMaterial);
 earthGroup.add(cloud);
 
+const moonGeometry = new THREE.SphereGeometry(earthObj.radiusMoon, objectsDefinition, objectsDefinition);
+const moonMaterial = new THREE.MeshBasicMaterial({
+    map: textureLoader.load(earthObj.textureMoon)
+})
+const moon = new THREE.Mesh(moonGeometry, moonMaterial);
+earthGroup.add(moon);
+
 scene.add(earthGroup);
+//-----------------------------------------------------------------------
+
+//mars
+const marsGeometry = new THREE.SphereGeometry(marsObj.radius, objectsDefinition, objectsDefinition);
+const marsMaterial = new THREE.MeshBasicMaterial({
+    map: textureLoader.load(marsObj.texture)
+})
+const mars = new THREE.Mesh(marsGeometry, marsMaterial);
+scene.add(mars);
+//-----------------------------------------------------------------------
+
+//jupiter group
+const jupiterGroup = new THREE.Group();
+
+const jupiterGeometry = new THREE.SphereGeometry(jupiterObj.radius, objectsDefinition, objectsDefinition);
+const jupiterMaterial = new THREE.MeshBasicMaterial({
+    map: textureLoader.load(jupiterObj.texture)
+});
+const jupiter = new THREE.Mesh(jupiterGeometry, jupiterMaterial);
+jupiterGroup.add(jupiter);
+
+scene.add(jupiterGroup);
+//-----------------------------------------------------------------------
+
+//saturn group
+const saturnGroup = new THREE.Group();
+
+const saturnGeometry = new THREE.SphereGeometry(saturnObj.radius, objectsDefinition, objectsDefinition);
+const saturnMaterial = new THREE.MeshBasicMaterial({
+    map: textureLoader.load(saturnObj.texture)
+});
+const saturn = new THREE.Mesh(saturnGeometry, saturnMaterial);
+saturnGroup.add(saturn);
+
+const ringGeometry = new THREE.RingGeometry(saturnObj.radiusRingInside, saturnObj.radiusRingOutside, objectsDefinition, objectsDefinition);
+const ringMaterial = new THREE.MeshBasicMaterial({
+    map: textureLoader.load(saturnObj.textureRing),
+    transparent: true,
+    side: THREE.DoubleSide
+});
+const ring = new THREE.Mesh(ringGeometry, ringMaterial);
+ring.rotation.x = Math.PI * 0.5;
+saturnGroup.add(ring);
+
+scene.add(saturnGroup);
+//-----------------------------------------------------------------------
+
+//uranus
+const uranusGeometry = new THREE.SphereGeometry(uranusObj.radius, objectsDefinition, objectsDefinition);
+const uranusMaterial = new THREE.MeshBasicMaterial({
+    map: textureLoader.load(uranusObj.texture)
+})
+const uranus = new THREE.Mesh(uranusGeometry, uranusMaterial);
+scene.add(uranus);
+//-----------------------------------------------------------------------
+
+//neptune
+const neptuneGeometry = new THREE.SphereGeometry(neptuneObj.radius, objectsDefinition, objectsDefinition);
+const neptuneMaterial = new THREE.MeshBasicMaterial({
+    map: textureLoader.load(neptuneObj.texture)
+})
+const neptune = new THREE.Mesh(neptuneGeometry, neptuneMaterial);
+scene.add(neptune);
 //-----------------------------------------------------------------------
 
 // OrbitControls
@@ -130,6 +234,10 @@ function animate() {
     //obj rotations
     sun.rotation.y += sunObj.rotation;
     cloud.rotation.y += earthObj.rotationCloud;
+
+    mercury.position.x = -Math.cos(time * 1) * 10;
+    mercury.position.z = -Math.sin(time * 1) * 10;
+    camera.lookAt(mercury.position.x,0,mercury.position.z);
 
     requestAnimationFrame(animate);
 
